bb113050719e99e6c8c7b17ed0b6ba4a21a3fc63
givikuna <thommaster10@gmail.com>
2025-01-10 22:55:15 -0500

    included coordinates field in BLock

--- simulation/classes/locations/Block.ts
+++ simulation/classes/locations/Block.ts
@@ -1,1 +1,17 @@
-export class Block {}
+import { Coordinates } from "../../../types";
+
+export class Block {
+    #coords: Coordinates;
+
+    constructor(coords: Coordinates) {
+        this.#coords = [coords[0], coords[1]];
+    }
+
+    get coords(): Coordinates {
+        return this.#coords;
+    }
+
+    set coords(coords: Coordinates) {
+        this.#coords = [coords[0], coords[1]];
+    }
+}
