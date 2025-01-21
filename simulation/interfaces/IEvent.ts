66f86629cca658313a583307526cc8e6a8d64c08
Larry <larryhe360@gmail.com>
2025-01-21 14:48:46 -0500

    hi

--- simulation/interfaces/IEvent.ts
+++ simulation/interfaces/IEvent.ts
@@ -1,9 +1,11 @@
 import { Person } from "../classes/Person";
+import { Comparable } from "./Comparable";
+import { Rememberable } from "./Rememberable";
 
-export interface IEvent {
-    discreteness: number;
-    visibility: number;
-    participants: Person[];
+export interface IEvent extends Rememberable, Comparable {
+    getDiscreteness(): number;
+    getVisibility(): number;
+    getParticipants(): string[];
 
     noticed(guy: Person): void;
     finished(): void;
